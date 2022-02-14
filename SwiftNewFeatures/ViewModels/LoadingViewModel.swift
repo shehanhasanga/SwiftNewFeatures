0a03bfd10dad81172a6c9648faedef04dcceb360
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-14 09:24:08 +0530

    add async await to load data

---
+++ SwiftNewFeatures/ViewModels/LoadingViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  LoadingViewModel.swift
+//  SwiftNewFeatures
+//
+//  Created by shehan karunarathna on 2022-02-14.
+//
+
+import Foundation
+class LoadingViewModel : ObservableObject{
+    @Published var users : [User] = []
+    let urlString:String = "https://jsonplaceholder.typicode.com/users"
+    
+    init() {
+
+//        await fetchUsers(urlString: urlString)
+    }
+    func fetchUsers() async{
+        let session = URLSession(configuration: .default)
+        guard let url = URL(string: urlString) else { return  }
+        do{
+           
+//            let task = try await session.data(from: url)
+            let urlRequest = URLRequest(url: url)
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
+
+            let users = try JSONDecoder().decode([User].self, from:data)
+            self.users = users
+        }catch let error{
+            print(error)
+        }
+    }
+}
+
