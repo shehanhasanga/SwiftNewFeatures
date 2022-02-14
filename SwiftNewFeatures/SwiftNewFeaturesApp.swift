343aafb9297273ddd63877f35c53d8cd751bb384
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-14 08:28:51 +0530

    Initial Commit

---
+++ SwiftNewFeatures/SwiftNewFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewFeaturesApp.swift
+//  SwiftNewFeatures
+//
+//  Created by shehan karunarathna on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
